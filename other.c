c2c6c4819e072bd5391a63d69175ed2689b7ea77
c <khilji@kth.se>
2021-09-21 13:03:42 +0500

    second commit

--- other.c
+++ other.c
@@ -6,8 +6,15 @@ int main()
 	printf("for more info pull changes from github\n");
 	int * y;
 	y = new int; 
+
 	//printf("here!");
         
+
+	printf("here!");
+	printf("there!");
+	printf("come!");
+
+
 
 	return 0;
 	
